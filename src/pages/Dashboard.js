a58b6b39fb85640c6587628fd85fc2f9b6aa6139
Caseykins <caseyjenkins92@gmail.com>
2020-11-23 17:50:56 -0700

    adds placeholder text/structure to our pages

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Dashboard = props => {
+  return (
+    <div>
+
+      <h1>Welcome, Fellow Advocate</h1>
+
+      <div className="container">
+
+        <div className="widget"> Sign-Ups
+          <h3>Below are other advocates for our cause:</h3>
+
+            <div>
+              <ul className="userList">
+                <li className='userInfo'>
+                  Cool person here.
+                </li>
+              </ul>
+            </div>
+
+        </div>
+
+        <div className="widget">
+
+        </div>
+
+        <div className="widget">
+          
+        </div>
+
+        <div className="widget">
+          
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard;
