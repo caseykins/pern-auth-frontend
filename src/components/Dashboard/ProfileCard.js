221454dde08e78123017e63a5d5551d44d91215f
Caseykins <caseyjenkins92@gmail.com>
2020-11-24 15:00:35 -0700

    more dashboard changes

--- src/components/Dashboard/ProfileCard.js
+++ src/components/Dashboard/ProfileCard.js
@@ -5,8 +5,11 @@ const ProfileCard = () => {
     return (
         <div className="ProfileCard">
             <div>
-                <h2 classname="dashTitle"
-                    id="line"> Dashboard </h2>
+                <h2 id="dashTitle"> Dashboard </h2>
+                <h3 id="dashLine"></h3>
+            </div>
+            <div className="profile">
+                <p> <span id="memberProfile">Member Profile</span> The information can be edited from your admin profile page.</p>
             </div>
         </div>
     );
